af20faf1ed03c649f908c46a2a1acd6a00b7f0be
beginsmauel <jbeginsamuel@gmail.com>
2022-01-25 20:00:57 +0000

    Login MFE added

---
+++ src/root.component.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Root(props) {
+  return (
+    <h1 class="flex flex-row justify-center p-16">
+    <p class="max-w-md">Login Code Here</p>
+  </h1>
+  );
+  
+}
